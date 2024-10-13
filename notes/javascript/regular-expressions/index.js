fae9f73c8c8a751e3ca08b03834e25753045807e
Piyush Patel <piyushpatel2005@gmail.com>
2024-10-12 23:02:22 -0400

    removed code file

--- notes/javascript/regular-expressions/index.js
+++
@@ -1,6 +0,0 @@
-// Write examples for Javascript regex flags
-const input = 'Hello World';
-const regex = /l+/g;
-console.log(input.match(regex)); // Output: ['ll', 'l']
-console.log(input.search(/h/i)); // Output: 0
-console.log("Hello WORLD".match(/l+/gi)); // Output: ['ll', 'L']
